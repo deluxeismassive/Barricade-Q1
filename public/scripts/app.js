5a2740208e7d5cc8db3b6c78d51b14e7ca84dcb5
Nathan Edlen <nathan.edlen@gmail.com>
2017-02-07 16:00:54 -0700

    added movement for walls and pieces

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,6 +9,7 @@ function generateGameBoard() {
       $('#tr'+ i).append($('<td></td>', {id: 'td'+j}))
     }
   }
+  $('.rulesWindow').hide()
 }
 
 function placeStartingPieces() {
@@ -32,6 +33,11 @@ function placeStartingWalls() {
 function placeTest() {
   $('#tr2 > #td2').addClass('uwallRyt')
   $('#tr3 > #td2').addClass('uwallBot')
+  $('#tr3 > #td2').append($('<div>', {class: 'upiece'}))
+  $('#tr3 > #td2').addClass('ywallRyt')
+  $('#tr3 > #td1').addClass('ywallRyt')
+  $('#tr2 > #td0').addClass('uwallRyt')
+  $('#tr4 > #td3').addClass('uwallRyt')
 }
 
 generateGameBoard();
@@ -40,25 +46,324 @@ placeStartingWalls();
 
 placeTest();
 
+//TopWall Movement Handlers
+$(document).on('click', '#TlowerRight', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  $('.selected').parent().prev().find('#'+$id).removeClass('uwallBot')
+  $('.selected').addClass('uwallRyt')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#TlowerLeft', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  $('.selected').parent().prev().find('#'+$id).removeClass('uwallBot')
+  $('.selected').prev().addClass('uwallRyt')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#TupperRight', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  $('.selected').removeClass('uwallBot')
+  $('.selected').addClass('uwallRyt')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#TupperLeft', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  var $pid = $('.selected').prev().attr('id')
+  $('.selected').removeClass('uwallBot')
+  $('.selected').prev().addClass('uwallRyt')
+  event.stopImmediatePropagation()
+})
+
+//RightWall event Handlers
+
+$(document).on('click', '#RupperLeft', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  $('.selected').removeClass('uwallRyt')
+  $('.selected').parent().prev().find('#'+$id).addClass('uwallBot')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#RupperRight', function (event) {
+  console.log('ping');
+  var $nid = $('.selected').attr('id')
+  $('.selected').prev().removeClass('uwallRyt')
+  $('.selected').parent().prev().find('#'+$nid).addClass('uwallBot')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#RlowerLeft', function (event) {
+  console.log('ping');
+  $('.selected').removeClass('uwallRyt')
+  $('.selected').addClass('uwallBot')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#RlowerRight', function (event) {
+  console.log('ping');
+  $('.selected').prev().removeClass('uwallRyt')
+  $('.selected').addClass('uwallBot')
+  event.stopImmediatePropagation()
+})
+
+//LeftWall movement event Handlers
+
+$(document).on('click', '#LupperLeft', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  $('.selected').removeClass('uwallRyt')
+  $('.selected').parent().prev().find('#'+$id).addClass('uwallBot')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#LupperRight', function (event) {
+  console.log('ping');
+  var $nid = $('.selected').attr('id')
+  $('.selected').prev().removeClass('uwallRyt')
+  $('.selected').parent().prev().find('#'+$nid).addClass('uwallBot')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#LlowerLeft', function (event) {
+  console.log('ping');
+  $('.selected').removeClass('uwallRyt')
+  $('.selected').addClass('uwallBot')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#LlowerRight', function (event) {
+  console.log('ping');
+  $('.selected').prev().removeClass('uwallRyt')
+  $('.selected').addClass('uwallBot')
+  event.stopImmediatePropagation()
+})
+
+
+//BottomWall movement event Handlers
+
+$(document).on('click', '#BlowerRight', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  $('.selected').parent().prev().find('#'+$id).removeClass('uwallBot')
+  $('.selected').addClass('uwallRyt')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#BlowerLeft', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  $('.selected').parent().prev().find('#'+$id).removeClass('uwallBot')
+  $('.selected').prev().addClass('uwallRyt')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#BupperRight', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  $('.selected').removeClass('uwallBot')
+  $('.selected').addClass('uwallRyt')
+  event.stopImmediatePropagation()
+})
+$(document).on('click', '#BupperLeft', function (event) {
+  console.log('ping');
+  var $id = $('.selected').attr('id')
+  var $pid = $('.selected').prev().attr('id')
+  $('.selected').removeClass('uwallBot')
+  $('.selected').prev().addClass('uwallRyt')
+  event.stopImmediatePropagation()
+})
 
 $('td').click(function(event) {
-  $('.choiceBox').empty()
   var $tar = $(this);
-  //console.log('ping');
-  if ($tar.children().is('.upiece')) {
-    $('.choiceBox').append($('<p>', {id: 'piecep'}).text('Select your piece'))
-  };
-  if ($tar.is('.uwallBot')) {
-    $('.choiceBox').append($('<p>', {id: 'wbotp'}).text('Select your bottom wall'))
-  }
-  if ($tar.is('.uwallRyt')) {
-    $('.choiceBox').append($('<p>', {id: 'wrytp'}).text('Select your right wall'))
-  }
-  if ($tar.prev().is('.uwallRyt'))  {
-    $('.choiceBox').append($('<p>', {id: 'wltfp'}).text('Select your left wall'))
-  }
-  var $id = ($tar.attr('id'))
-  if ($tar.parent().prev().find('#'+$id).is('.uwallBot')) {
-    $('.choiceBox').append($('<p>', {id: 'wtopp'}).text('Select your top wall'))
+  if ($tar.is('.highlightCellPiece')) {
+    movePiece($tar)
+  } else {
+    $('td').removeClass('selected').removeClass('highlightCellPiece').removeClass('botwallchange').removeClass('rytwallchange').children().removeClass('upiecechange')
+    $('.indicateRight').remove()
+    $('.indicateLeft').remove()
+    $('.indicateUp').remove()
+    $('.indicateDown').remove()
+    $('.choiceBox').empty()
+    $tar.addClass('selected')
+    if ($tar.children().is('.upiece')) {
+      $('.choiceBox').append($('<p>', {id: 'piecep'}).text('Select your piece'))
+    };
+    if ($tar.is('.uwallBot')) {
+      $('.choiceBox').append($('<p>', {id: 'wbotp'}).text('Select your bottom wall'))
+    }
+    if ($tar.is('.uwallRyt')) {
+      $('.choiceBox').append($('<p>', {id: 'wrytp'}).text('Select your right wall'))
+    }
+    if ($tar.prev().is('.uwallRyt'))  {
+      $('.choiceBox').append($('<p>', {id: 'wlftp'}).text('Select your left wall'))
+    }
+    var $id = ($tar.attr('id'))
+    if ($tar.parent().prev().find('#'+$id).is('.uwallBot')) {
+      $('.choiceBox').append($('<p>', {id: 'wtopp', class: 'choice'}).text('Select your top wall'))
+    }
   }
 })
+
+// $('#wtopp').click(function(event) {
+//   var $id = $('.selected').attr('id')
+//   $('.selected').parent().prev().find('#'+id).addClass('botwallchange')
+// })
+
+
+//Choice Box selection of TopWall and the highlighting of potential moves.
+$('.choiceBox').on('click', '#wtopp', function(event) {
+  $('.indicateRight').remove()
+  $('.indicateLeft').remove()
+  $('.indicateUp').remove()
+  $('.indicateDown').remove()
+  var $id = $('.selected').attr('id')
+  $('td').removeClass('botwallchange').removeClass('rytwallchange').children().removeClass('upiecechange')
+  $('.selected').parent().prev().find('#'+$id).addClass('botwallchange')
+
+  if (!$('.selected').filter('.ywallRyt').length && !$('.selected').filter('.uwallRyt').length) {
+    console.log('ping');
+    $('.selected').append($('<div>', {class: 'indicateRight', id: 'TlowerRight'}))
+  }
+
+  if (!$('.selected').prev().filter('.ywallRyt').length && !$('.selected').prev().filter('.uwallRyt').length) {
+    console.log('ping');
+    $('.selected').append($('<div>', {class: 'indicateLeft', id: 'TlowerLeft'}))
+  }
+
+  var $id = $('.selected').attr('id')
+  var $idp = $('.selected').prev().attr('id')
+  if (!$('.selected').parent().prev().find('#'+$id).filter('.ywallRyt').length && !$('.selected').parent().prev().find('#'+$id).filter('.uwallRyt').length) {
+    console.log('pong');
+    $('.selected').parent().prev().find('#'+$id).append($('<div>', {class: 'indicateRight', id: 'TupperRight'}))
+  }
+  if (!$('.selected').parent().prev().find('#'+$idp).filter('.ywallRyt').length && !$('.selected').parent().prev().find('#'+$idp).filter('.uwallRyt').length) {
+    console.log('pongpong');
+    $('.selected').parent().prev().find('#'+$id).append($('<div>', {class: 'indicateLeft', id: 'TupperLeft'}))
+  }
+})
+
+//Choice Box selection of RightWall and the highlighting of potential moves.
+$('.choiceBox').on('click', '#wrytp', function(event) {
+  $('.indicateRight').remove()
+  $('.indicateLeft').remove()
+  $('.indicateUp').remove()
+  $('.indicateDown').remove()
+  $('td').removeClass('botwallchange').removeClass('rytwallchange').children().removeClass('upiecechange')
+  $('.selected').addClass('rytwallchange')
+
+  if (!$('.selected').filter('.ywallBot').length && !$('.selected').filter('.uwallBot').length) {
+    console.log('ping');
+    $('.selected').append($('<div>', {class: 'indicateDown', id: 'RlowerLeft'}))
+  }
+
+  var $id = $('.selected').attr('id')
+  var $nid = $('.selected').next().attr('id')
+  if (!$('.selected').parent().prev().find('#'+$id).filter('.ywallBot').length && !$('.selected').parent().prev().find('#'+$id).filter('.uwallBot').length) {
+    console.log('ping');
+    $('.selected').append($('<div>', {class: 'indicateUp', id: 'RupperLeft'}))
+  }
+
+  if (!$('.selected').parent().prev().find('#'+$nid).filter('.ywallBot').length && !$('.selected').parent().prev().find('#'+$nid).filter('.uwallBot').length) {
+    console.log('ping');
+    $('.selected').next().append($('<div>', {class: 'indicateUp', id: 'RupperRight'}))
+  }
+
+  if (!$('.selected').next().filter('.ywallBot').length && !$('.selected').next().filter('.uwallBot').length) {
+    console.log('ping');
+    $('.selected').next().append($('<div>', {class: 'indicateDown', id: 'RlowerRight'}))
+  }
+})
+//Choice Box selection of LeftWall and the highlighting of potential moves.
+$('.choiceBox').on('click', '#wlftp', function(event) {
+  $('.indicateRight').remove()
+  $('.indicateLeft').remove()
+  $('.indicateUp').remove()
+  $('.indicateDown').remove()
+  $('td').removeClass('botwallchange').removeClass('rytwallchange').children().removeClass('upiecechange')
+  $('.selected').prev().addClass('rytwallchange')
+
+  if (!$('.selected').filter('.ywallBot').length && !$('.selected').filter('.uwallBot').length) {
+    console.log('ping');
+    $('.selected').append($('<div>', {class: 'indicateDown', id: 'LlowerRight'}))
+  }
+
+  var $id = $('.selected').attr('id')
+  var $pid = $('.selected').prev().attr('id')
+  if (!$('.selected').parent().prev().find('#'+$id).filter('.ywallBot').length && !$('.selected').parent().prev().find('#'+$id).filter('.uwallBot').length) {
+    console.log('ping');
+    $('.selected').append($('<div>', {class: 'indicateUp', id: 'LupperRight'}))
+  }
+
+  if (!$('.selected').parent().prev().find('#'+$pid).filter('.ywallBot').length && !$('.selected').parent().prev().find('#'+$pid).filter('.uwallBot').length) {
+    console.log('ping');
+    $('.selected').prev().append($('<div>', {class: 'indicateUp', id: 'LupperLeft'}))
+  }
+
+  if (!$('.selected').prev().filter('.ywallBot').length && !$('.selected').prev().filter('.uwallBot').length) {
+    console.log('ping');
+    $('.selected').prev().append($('<div>', {class: 'indicateDown', id: 'LlowerLeft'}))
+  }
+})
+
+//Choice Box selection of BottomWall and the highlighting of potential moves.
+$('.choiceBox').on('click', '#wbotp', function(event) {
+  $('.indicateRight').remove()
+  $('.indicateLeft').remove()
+  $('.indicateUp').remove()
+  $('.indicateDown').remove()
+  $('td').removeClass('botwallchange').removeClass('rytwallchange').children().removeClass('upiecechange')
+  $('.selected').addClass('botwallchange')
+
+  if (!$('.selected').filter('.ywallRyt').length && !$('.selected').filter('.uwallRyt').length) {
+    console.log('ping');
+    $('.selected').append($('<div>', {class: 'indicateRight', id: 'BupperRight'}))
+  }
+
+  if (!$('.selected').prev().filter('.ywallRyt').length && !$('.selected').prev().filter('.uwallRyt').length) {
+    console.log('ping');
+    $('.selected').append($('<div>', {class: 'indicateLeft', id: 'BupperLeft'}))
+  }
+
+  var $id = $('.selected').attr('id')
+  var $idp = $('.selected').prev().attr('id')
+  if (!$('.selected').parent().next().find('#'+$id).filter('.ywallRyt').length && !$('.selected').parent().next().find('#'+$id).filter('.uwallRyt').length) {
+    console.log('pong');
+    $('.selected').parent().next().find('#'+$id).append($('<div>', {class: 'indicateRight', id: 'BlowerRight'}))
+  }
+  if (!$('.selected').parent().next().find('#'+$idp).filter('.ywallRyt').length && !$('.selected').parent().next().find('#'+$idp).filter('.uwallRyt').length) {
+    console.log('pongpong');
+    $('.selected').parent().next().find('#'+$id).append($('<div>', {class: 'indicateLeft', id: 'BlowerLeft'}))
+  }
+})
+
+//Choice Box selection of Piece and the highlighting of potential moves.
+$('.choiceBox').on('click', '#piecep', function(event) {
+  $('.indicateRight').remove()
+  $('.indicateLeft').remove()
+  $('.indicateUp').remove()
+  $('.indicateDown').remove()
+  $('td').removeClass('botwallchange').removeClass('rytwallchange').children().removeClass('upiecechange')
+  $('.selected').children().addClass('upiecechange')
+
+  if (!$('.selected').prev().find('div').length && !$('.selected').prev().filter('.ywallRyt').length) {
+    $('.selected').prev().addClass('highlightCellPiece')
+  }
+
+  if (!$('.selected').next().find('div').length && !$('.selected').filter('.ywallRyt').length) {
+    $('.selected').next().addClass('highlightCellPiece')
+  }
+
+  var $id = $('.selected').attr('id')
+  if (!$('.selected').parent().prev().find('#'+$id).find('div').length && !$('.selected').parent().prev().find('#'+$id).filter('.ywallBot').length) {
+    $('.selected').parent().prev().find('#'+$id).addClass('highlightCellPiece')
+  }
+
+  if (!$('.selected').parent().next().find('#'+$id).find('div').length && !$('.selected').filter('.ywallBot').length) {
+    $('.selected').parent().next().find('#'+$id).addClass('highlightCellPiece')
+  }
+})
+
+//Highlighted Piece destination.  Moving a peice to that destination.
+function movePiece($dest) {
+  $('.selected').addClass('.degeneratePiece')
+  $('.selected').empty()
+  console.log($dest);
+  $dest.append($('<div>', {class: 'upiece'}))
+  $('td').removeClass('selected').removeClass('highlightCellPiece').removeClass('botwallchange').removeClass('rytwallchange').children().removeClass('upiecechange')
+  $('.choiceBox').empty()
+}
